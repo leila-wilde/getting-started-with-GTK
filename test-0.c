bf6eaa88746412f09d51fdbc83c130859c3559a0
nyxx <leila.wilde@laplateforme.io>
2025-04-22 14:45:40 +0100

    modified example-0

--- test-0.c
+++ test-0.c
@@ -7,8 +7,8 @@ activate (GtkApplication* app,
   GtkWidget *window;
 
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_window_set_title (GTK_WINDOW (window), "GTK experiment");
+  gtk_window_set_default_size (GTK_WINDOW (window), 1800, 600);
   gtk_window_present (GTK_WINDOW (window));
 }
 
@@ -19,7 +19,7 @@ main (int    argc,
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("com.github.leila-wilde.example", 0);
+  app = gtk_application_new ("com.github.leila-wilde.test-0", 0);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
